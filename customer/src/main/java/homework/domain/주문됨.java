ee0f88b89ace30c193c0399000e5d2a8dd40cdee
ttuck000 <119825867+ttuck000@users.noreply.github.com>
2022-12-06 16:46:05 +0900

    commit

--- customer/src/main/java/homework/domain/주문됨.java
+++ customer/src/main/java/homework/domain/주문됨.java
@@ -1,11 +1,11 @@
 package homework.domain;
 
-import homework.domain.*;
 import homework.infra.AbstractEvent;
-import lombok.*;
+import lombok.Data;
 import java.util.*;
+
+
 @Data
-@ToString
 public class 주문됨 extends AbstractEvent {
 
     private Long orderid;
@@ -17,5 +17,3 @@ public class 주문됨 extends AbstractEvent {
     private String customername;
     private String orderstatus;
 }
-
-
